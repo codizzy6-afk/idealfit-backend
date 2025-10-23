0c29282a10a39b3f0557b7d3a1099a20cb6cdb4f
rahul <locks.legend@gmail.com>
2025-10-23 14:44:56 +0530

    Add CORS support to fix dashboard API connection issues

--- idealfit-backend/simple-api-server.js
+++ idealfit-backend/simple-api-server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
